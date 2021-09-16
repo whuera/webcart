d071768c971a3cdc5776f38bb2d7c78e7bad5cdb
williamhuerapeya <william.huera.ext@pedidosya.com>
2021-09-16 16:23:32 -0500

    update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   };
 
   consultarApi = () => {
-    const baseUrl = "http://3.137.165.30:8080/api/customer/customers";
+    const baseUrl = "http://18.191.14.125:8080/api/customer/customers";
     fetch(baseUrl)
       .then((respuesta) => respuesta.json())
       .then((resultado) => this.setState({ users: resultado }));
